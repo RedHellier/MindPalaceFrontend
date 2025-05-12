24d76a27ee95b0864c3eca41008000c01feffb13
RedHellier <redhellier@gmail.com>
2025-05-12 06:36:35 +0100

    Topic titles displaying on topic page after redirect

---
+++ src/app/topics/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { supabase } from "../../supabaseClient";
+const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL!;
+
+type Topic = {
+  created_at: string;
+  id: string;
+  title: string;
+};
+
+export default function Topics() {
+  const [topics, setTopics] = useState([]);
+
+  const getTopics = async () => {
+    const { data: sessionData } = await supabase.auth.getSession();
+    const accessToken = sessionData.session?.access_token;
+
+    const data = await fetch(`${backendURL}/topic/`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }).then(async (res) => {
+      return await res.json();
+    });
+    console.log(data);
+    setTopics(data);
+  };
+
+  useEffect(() => {
+    getTopics();
+  }, []);
+
+  return (
+    <div>
+      <h1>Topics Page</h1>
+      <ul className="">
+        {topics.map((topic: Topic) => (
+          <li key={topic.id} className="text-right ml-5">
+            {topic.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
